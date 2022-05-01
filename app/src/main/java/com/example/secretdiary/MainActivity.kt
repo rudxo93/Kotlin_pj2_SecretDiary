caf61324bbf17f9fe81d4c5d6353d4643d605f27
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-05-01 12:11:36 +0900

    numberPicker에 숫자 0~9

--- app/src/main/java/com/example/secretdiary/MainActivity.kt
+++ app/src/main/java/com/example/secretdiary/MainActivity.kt
@@ -2,8 +2,31 @@ package com.example.secretdiary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.NumberPicker
 
 class MainActivity : AppCompatActivity() {
+
+    private val numberPicker1: NumberPicker by lazy {
+        findViewById<NumberPicker>(R.id.numberPicker1).apply {
+            minValue = 0
+            maxValue = 9
+        }
+    }
+
+    private val nubmerPicker2: NumberPicker by lazy {
+        findViewById<NumberPicker>(R.id.numberPicker2).apply {
+            minValue = 0
+            maxValue = 9
+        }
+    }
+
+    private val numberPicker3: NumberPicker by lazy {
+        findViewById<NumberPicker>(R.id.numberPicker3).apply {
+            minValue = 0
+            maxValue = 9
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
